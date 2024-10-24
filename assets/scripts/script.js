472a86aa170c54ff8f3996de25abf102f74b1d2a
Heunsig <heun3344@gmail.com>
2024-10-24 22:57:15 +0900

    docs: 주석 추가

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -2,7 +2,7 @@ function createTodoItem(value) {
   const noteItemContent = $("<div/>", {
     class: 'note-item__content',
     text: value
-  })
+  });
 
   const noteItem = $("<li/>", {
     class: 'note-item',
@@ -18,7 +18,7 @@ $(document).ready(function() {
 
   const form = $('[data-form]');
   const input = $('[data-input]');
-  const noteList = $('[data-note-list]')
+  const noteList = $('[data-note-list]');
   const noteCount = $('[data-note-count]');
 
   form.submit(e => {
