a1b6c091eb53fe6f24a7f66f59b2432c3013bcb3
Heunsig <heun3344@gmail.com>
2024-10-25 10:37:16 +0900

    docs: add comments

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,4 @@
-function createTodoItem(value) {
+function createNoteItem(value) {
   const noteItemContent = $("<div/>", {
     class: 'note-item__content',
     text: value
@@ -14,7 +14,6 @@ function createTodoItem(value) {
 }
 
 $(document).ready(function() {
-  console.log('hihi22')
   const notes = [];
 
   const form = $('[data-form]');
@@ -30,8 +29,16 @@ $(document).ready(function() {
       return;
     }
 
+    // 신규 Note element 생성
+    const newNoteItemElement = createNoteItem(value);
+
+    // DOM 업데이트: 신규 Note element를 NOTE List 에 추가
+    noteList.append(newNoteItemElement);
+
+    // 신규 Note 데이터 추가
     notes.push(value);
-    noteList.append(createTodoItem(value));
+    
+    // DOM 업데이트: 메모 카운트 화면에 반영
     noteCount.text(notes.length);
 
     input.val('');
